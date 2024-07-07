a02af953f292237a474e9df6ccb37669ca8a362a
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-07-07 14:25:35 +0300

    commit

---
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.shape({
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string,
+  }),
+};
+
+export function ShopItemFunc({item}) {
+  const { brand,
+    title,
+    description,
+    descriptionFull,
+    price,
+    currency } = item;
+  return (
+    <div>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
